bfac56ae64a56b531ab2ca21b6ea389cfde66fa2
Antoine Cornaz <antomax33.jeux@gmail.com>
2024-03-23 13:00:17 +0100

    adding login test on exampleInstrumentedTest to try CI

--- app/src/androidTest/java/com/github/se/polyfit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/se/polyfit/ExampleInstrumentedTest.kt
@@ -1,8 +1,14 @@
 package com.github.se.polyfit
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.github.se.polyfit.ui.screen.HomeScreen
+import com.github.se.polyfit.ui.screen.LoginScreen
+import io.github.kakaocup.compose.node.element.ComposeScreen
 import org.junit.Assert.*
+import org.junit.Ignore
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,6 +19,41 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+  @Test
+  fun titleAndButtonAreCorrectlyDisplayed() {
+    ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
+      // Test the UI elements
+      loginTitle {
+        assertIsDisplayed()
+        assertTextEquals("Welcome")
+      }
+      loginButton {
+        assertIsDisplayed()
+        assertHasClickAction()
+      }
+    }
+  }
+
+  @Ignore("This test is not working, It need a mock authentication service to work.")
+  @Test
+  fun googleSignInReturnsValidActivityResult() {
+    ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
+      // composeTestRule.setContent { LoginScreen(nav) }
+      loginButton {
+        assertIsDisplayed()
+        performClick()
+      }
+      // assert that an Intent resolving to Google Mobile Services has been sent (for sign-in)
+      // intended(toPackage("com.google.android.gms"))
+    }
+
+    ComposeScreen.onComposeScreen<HomeScreen>(composeTestRule) {
+      // Test the UI elements
+      homeScreen { assertIsDisplayed() }
+    }
+  }
   @Test
   fun useAppContext() {
     // Context of the app under test.
