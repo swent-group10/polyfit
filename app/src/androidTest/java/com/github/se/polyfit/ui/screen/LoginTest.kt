46915081a43694d188748abdef55720dcd015538
RandomUsername1315 <charles.rousset@epfl.ch>
2024-03-27 13:41:47 +0100

    Add test intent for login
    Add test to verify successful Google sign-in resul

--- app/src/androidTest/java/com/github/se/polyfit/ui/screen/LoginTest.kt
+++ app/src/androidTest/java/com/github/se/polyfit/ui/screen/LoginTest.kt
@@ -1,10 +1,9 @@
 package com.github.se.polyfit.ui.screen
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.performClick
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.polyfit.MainActivity
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
@@ -15,35 +14,51 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LoginTest : TestCase() {
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
-  @Test
-  fun titleAndButtonAreCorrectlyDisplayed() {
-    ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
-      // Test the UI elements
-      loginTitle {
-        assertIsDisplayed()
-        assertTextEquals("Welcome")
-      }
-      loginButton {
-        assertIsDisplayed()
-        assertHasClickAction()
-      }
+    @Test
+    fun titleAndButtonAreCorrectlyDisplayed() {
+        ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
+            // Test the UI elements
+            loginTitle {
+                assertIsDisplayed()
+                assertTextEquals("Welcome")
+            }
+            loginButton {
+                assertIsDisplayed()
+                assertHasClickAction()
+            }
+        }
     }
-  }
 
-  @Test
-  fun loginButtonNavigatesToNewScreen() {
-    // Launch the LoginScreen
-    ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
-      // Test the UI elements
-      loginButton {
-        assertIsDisplayed()
-        assertHasClickAction()
-        performClick()
-      }
-      // TODO check we are on another page
-      Intents.intended(IntentMatchers.toPackage("com.google.android.gms"))
+    @Test
+    fun loginButtonNavigatesToNewScreen() {
+        // Launch the LoginScreen
+        ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
+            // Test the UI elements
+            loginButton {
+                assertIsDisplayed()
+                assertHasClickAction()
+                performClick()
+            }
+            // TODO check we are on another page
+            intended(toPackage("com.google.android.gms"))
+        }
     }
-  }
+
+    @Test
+    fun googleSignInReturnsValidActivityResult() {
+        ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
+            loginButton {
+                assertIsDisplayed()
+                performClick()
+            }
+
+            // assert that an Intent resolving to Google Mobile Services has been sent (for sign-in)
+            intended(toPackage("com.google.android.gms"))
+        }
+    }
 }
