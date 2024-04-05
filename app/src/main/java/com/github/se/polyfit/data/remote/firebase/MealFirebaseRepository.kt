0116b06c9dc25897955b7c8aa35f61df5ebe2505
RandomUsername1315 <charles.rousset@epfl.ch>
2024-04-05 21:00:08 +0200

    kftm

--- app/src/main/java/com/github/se/polyfit/data/remote/firebase/MealFirebaseRepository.kt
+++ app/src/main/java/com/github/se/polyfit/data/remote/firebase/MealFirebaseRepository.kt
@@ -7,77 +7,71 @@ import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MealFirebaseRepository(
-    private val userId: String, private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val userId: String,
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 ) {
 
-    private val mealCollection = db.collection("users").document(userId).collection("meals")
+  private val mealCollection = db.collection("users").document(userId).collection("meals")
 
-
-    fun storeMeal(meal: Meal): Task<DocumentReference> {
-        val mealData = Meal.serialize(meal)
-        return if (meal.firebasaeId.isNotEmpty()) {
-            mealCollection.document(meal.firebasaeId).set(mealData).continueWith { task ->
-                if (task.isSuccessful) {
-                    mealCollection.document(meal.firebasaeId)
-                } else {
-                    throw task.exception!!
-                }
-            }
+  fun storeMeal(meal: Meal): Task<DocumentReference> {
+    val mealData = Meal.serialize(meal)
+    return if (meal.firebasaeId.isNotEmpty()) {
+      mealCollection.document(meal.firebasaeId).set(mealData).continueWith { task ->
+        if (task.isSuccessful) {
+          mealCollection.document(meal.firebasaeId)
         } else {
-            mealCollection.add(mealData).continueWith { task ->
-                if (task.isSuccessful) {
-                    meal.firebasaeId = task.result!!.id
-                    task.result
-                } else {
-                    throw task.exception!!
-                }
-            }
+          throw task.exception!!
         }
-    }
-
-    // maybe ok for now, but in the future will need to find a better way to
-    // distinguish between meals
-    fun getMeal(firebaseID: String): Task<Meal> {
-
-        return getAllMeals().continueWith { task ->
-            Log.d("MealFirebaseRepository", "getMeal: $firebaseID")
-            if (task.isSuccessful) {
-
-                task.result?.find { it.firebasaeId == firebaseID }
-            } else {
-                throw Exception("Failed to fetch meals : " + task.exception?.message)
-            }
+      }
+    } else {
+      mealCollection.add(mealData).continueWith { task ->
+        if (task.isSuccessful) {
+          meal.firebasaeId = task.result!!.id
+          task.result
+        } else {
+          throw task.exception!!
         }
+      }
     }
+  }
 
-    fun getAllMeals(): Task<List<Meal>> {
-        val task = mealCollection.get()
+  // maybe ok for now, but in the future will need to find a better way to
+  // distinguish between meals
+  fun getMeal(firebaseID: String): Task<Meal> {
 
+    return getAllMeals().continueWith { task ->
+      Log.d("MealFirebaseRepository", "getMeal: $firebaseID")
+      if (task.isSuccessful) {
 
-        return task.continueWith { task ->
-            if (task.isSuccessful && task.result != null) {
-                try {
-                    // Convert documents to Meal objects
-                    task.result.documents.mapNotNull { document ->
-                        document.data?.let {
-                            Meal.deserialize(it).apply {
-                                this!!.firebasaeId = document.id
-                            }
-                        }
-                    }
-                } catch (e: Exception) {
-                    Log.e("MealFirebaseRepository", "Error processing meal documents", e)
-                    listOf<Meal>()
-                }
-            } else {
-                Log.e("MealFirebaseRepository", "Failed to fetch meals: ${task.exception?.message}")
-                throw Exception("Failed to fetch meals: ${task.exception?.message}")
-            }
+        task.result?.find { it.firebasaeId == firebaseID }
+      } else {
+        throw Exception("Failed to fetch meals : " + task.exception?.message)
+      }
+    }
+  }
+
+  fun getAllMeals(): Task<List<Meal>> {
+    val task = mealCollection.get()
+
+    return task.continueWith { task ->
+      if (task.isSuccessful && task.result != null) {
+        try {
+          // Convert documents to Meal objects
+          task.result.documents.mapNotNull { document ->
+            document.data?.let { Meal.deserialize(it).apply { this!!.firebasaeId = document.id } }
+          }
+        } catch (e: Exception) {
+          Log.e("MealFirebaseRepository", "Error processing meal documents", e)
+          listOf<Meal>()
         }
+      } else {
+        Log.e("MealFirebaseRepository", "Failed to fetch meals: ${task.exception?.message}")
+        throw Exception("Failed to fetch meals: ${task.exception?.message}")
+      }
     }
+  }
 
-    fun deleteMeal(firebaseID: String): Task<Void> {
-        return mealCollection.document(firebaseID).delete()
-    }
-
+  fun deleteMeal(firebaseID: String): Task<Void> {
+    return mealCollection.document(firebaseID).delete()
+  }
 }
--- app/src/test/java/com/github/se/polyfit/model/ingredient/IngredientTest.kt
+++ app/src/test/java/com/github/se/polyfit/model/ingredient/IngredientTest.kt
@@ -4,125 +4,118 @@ import android.util.Log
 import com.github.se.polyfit.model.nutritionalInformation.NutritionalInformation
 import io.mockk.every
 import io.mockk.mockkStatic
+import kotlin.test.assertFailsWith
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import kotlin.test.assertFailsWith
 
 class IngredientTest {
-    private val ingredient = Ingredient("eggs", 1, NutritionalInformation(mutableListOf()))
+  private val ingredient = Ingredient("eggs", 1, NutritionalInformation(mutableListOf()))
 
-    @Before
-    fun setup() {
-        mockkStatic(Log::class)
-        every { Log.e(any(), any()) } returns 0
-        every { Log.e(any(), any(), any()) } returns 0
+  @Before
+  fun setup() {
+    mockkStatic(Log::class)
+    every { Log.e(any(), any()) } returns 0
+    every { Log.e(any(), any(), any()) } returns 0
+  }
+
+  @Test
+  fun serializeIngredient() {
+
+    val map = Ingredient.serialize(ingredient)
+    val expectedMap =
+        mapOf(
+            "name" to "eggs",
+            "id" to 1,
+            "nutritionalInformation" to
+                NutritionalInformation.serialize(ingredient.nutritionalInformation))
+
+    assert(map == expectedMap)
+  }
+
+  @Test
+  fun deserializeIngredient() {
+    Ingredient.serialize(ingredient).also { serializedIngredient ->
+      val deserializedIngredient = Ingredient.deserialize(serializedIngredient)
+      assert(ingredient == deserializedIngredient)
     }
+  }
+  // add more tests
 
-    @Test
-    fun serializeIngredient() {
+  @Test
+  fun testIngredientName() {
+    assert(ingredient.name == "eggs")
+  }
 
-        val map = Ingredient.serialize(ingredient)
-        val expectedMap =
-            mapOf(
-                "name" to "eggs",
-                "id" to 1,
-                "nutritionalInformation" to
-                        NutritionalInformation.serialize(ingredient.nutritionalInformation)
-            )
+  @Test
+  fun testSerializationDeserialization() {
+    val serializedIngredient = Ingredient.serialize(ingredient)
+    val deserializedIngredient = Ingredient.deserialize(serializedIngredient)
+    assert(ingredient == deserializedIngredient)
+    assert(ingredient.name == deserializedIngredient.name)
+    assert(ingredient.nutritionalInformation == deserializedIngredient.nutritionalInformation)
+  }
 
-        assert(map == expectedMap)
-    }
+  @Test
+  fun testSerializationDeserializationWithDefaultValues() {
+    // Create an Ingredient object with null values
+    val ingredientWithDefaultValues = Ingredient("eggs", 1, NutritionalInformation(mutableListOf()))
 
-    @Test
-    fun deserializeIngredient() {
-        Ingredient.serialize(ingredient).also { serializedIngredient ->
-            val deserializedIngredient = Ingredient.deserialize(serializedIngredient)
-            assert(ingredient == deserializedIngredient)
-        }
-    }
-    // add more tests
+    // Serialize the Ingredient object
+    val serializedIngredient = Ingredient.serialize(ingredientWithDefaultValues)
 
-    @Test
-    fun testIngredientName() {
-        assert(ingredient.name == "eggs")
-    }
+    // Deserialize the serialized Ingredient object
+    val deserializedIngredient = Ingredient.deserialize(serializedIngredient)
 
-    @Test
-    fun testSerializationDeserialization() {
-        val serializedIngredient = Ingredient.serialize(ingredient)
-        val deserializedIngredient = Ingredient.deserialize(serializedIngredient)
-        assert(ingredient == deserializedIngredient)
-        assert(ingredient.name == deserializedIngredient.name)
-        assert(ingredient.nutritionalInformation == deserializedIngredient.nutritionalInformation)
-    }
+    // Check if the deserialized Ingredient object is null
+    assert(deserializedIngredient == ingredientWithDefaultValues)
+  }
 
-    @Test
-    fun testSerializationDeserializationWithDefaultValues() {
-        // Create an Ingredient object with null values
-        val ingredientWithDefaultValues =
-            Ingredient("eggs", 1, NutritionalInformation(mutableListOf()))
+  @Test
+  fun testDeserializationWithEmptyValues() {
+    // Create a map with null values
+    val map = mapOf("name" to "eggs", "nutritionalInformation" to "")
 
-        // Serialize the Ingredient object
-        val serializedIngredient = Ingredient.serialize(ingredientWithDefaultValues)
+    // Deserialize the map
+    assertFailsWith<Exception> { Ingredient.deserialize(map) }
+  }
 
-        // Deserialize the serialized Ingredient object
-        val deserializedIngredient = Ingredient.deserialize(serializedIngredient)
+  @Test
+  fun deserializeIngredient_withValidData_returnsIngredient() {
+    val data =
+        mapOf("name" to "eggs", "id" to 1, "nutritionalInformation" to listOf<Map<String, Any>>())
 
-        // Check if the deserialized Ingredient object is null
-        assert(deserializedIngredient == ingredientWithDefaultValues)
-    }
+    val result = Ingredient.deserialize(data)
 
-    @Test
-    fun testDeserializationWithEmptyValues() {
-        // Create a map with null values
-        val map = mapOf("name" to "eggs", "nutritionalInformation" to "")
+    assertEquals("eggs", result.name)
+    assertEquals(1, result.id)
+    assertTrue(result.nutritionalInformation.nutrients.isEmpty())
+  }
 
-        // Deserialize the map
-        assertFailsWith<Exception> { Ingredient.deserialize(map) }
-    }
+  @Test
+  fun deserializeIngredient_withInvalidName_throwsException() {
+    val data =
+        mapOf("name" to 123, "id" to 1, "nutritionalInformation" to listOf<Map<String, Any>>())
 
-    @Test
-    fun deserializeIngredient_withValidData_returnsIngredient() {
-        val data =
-            mapOf(
-                "name" to "eggs",
-                "id" to 1,
-                "nutritionalInformation" to listOf<Map<String, Any>>()
-            )
+    assertFailsWith<IllegalArgumentException> { Ingredient.deserialize(data) }
+  }
 
-        val result = Ingredient.deserialize(data)
+  @Test
+  fun deserializeIngredient_withInvalidId_throwsException() {
+    val data =
+        mapOf(
+            "name" to "eggs",
+            "id" to "invalid",
+            "nutritionalInformation" to listOf<Map<String, Any>>())
 
-        assertEquals("eggs", result.name)
-        assertEquals(1, result.id)
-        assertTrue(result.nutritionalInformation.nutrients.isEmpty())
-    }
+    assertFailsWith<IllegalArgumentException> { Ingredient.deserialize(data) }
+  }
 
-    @Test
-    fun deserializeIngredient_withInvalidName_throwsException() {
-        val data =
-            mapOf("name" to 123, "id" to 1, "nutritionalInformation" to listOf<Map<String, Any>>())
+  @Test
+  fun deserializeIngredient_withInvalidNutritionalInformation_throwsException() {
+    val data = mapOf("name" to "eggs", "id" to 1, "nutritionalInformation" to "invalid")
 
-        assertFailsWith<IllegalArgumentException> { Ingredient.deserialize(data) }
-    }
-
-    @Test
-    fun deserializeIngredient_withInvalidId_throwsException() {
-        val data =
-            mapOf(
-                "name" to "eggs",
-                "id" to "invalid",
-                "nutritionalInformation" to listOf<Map<String, Any>>()
-            )
-
-        assertFailsWith<IllegalArgumentException> { Ingredient.deserialize(data) }
-    }
-
-    @Test
-    fun deserializeIngredient_withInvalidNutritionalInformation_throwsException() {
-        val data = mapOf("name" to "eggs", "id" to 1, "nutritionalInformation" to "invalid")
-
-        assertFailsWith<IllegalArgumentException> { Ingredient.deserialize(data) }
-    }
+    assertFailsWith<IllegalArgumentException> { Ingredient.deserialize(data) }
+  }
 }
