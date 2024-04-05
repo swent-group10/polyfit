2e3ab45764d7a05ea1bc560d5b087af407f7ddd2
RandomUsername1315 <charles.rousset@epfl.ch>
2024-04-05 15:17:55 +0200

    Second Test Imp

--- app/src/androidTest/java/com/github/se/polyfit/data/remote/MealFirebaseRepositoryTest.kt
+++ app/src/androidTest/java/com/github/se/polyfit/data/remote/MealFirebaseRepositoryTest.kt
@@ -1,12 +1,9 @@
 import com.github.se.polyfit.data.remote.firebase.MealFirebaseRepository
-import com.github.se.polyfit.model.ingredient.Ingredient
 import com.github.se.polyfit.model.meal.Meal
 import com.github.se.polyfit.model.meal.MealOccasion
 import com.github.se.polyfit.model.nutritionalInformation.NutritionalInformation
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
@@ -18,114 +15,95 @@ import org.junit.Test
 
 class MealFirebaseRepositoryTest {
 
-    private lateinit var db: MealFirebaseRepository
-    private lateinit var firestore: FirebaseFirestore
-    private lateinit var meal: Meal
-    private lateinit var task: Task<QuerySnapshot>
+  private lateinit var db: MealFirebaseRepository
+  private lateinit var firestore: FirebaseFirestore
+  private lateinit var meal: Meal
+  private lateinit var task: Task<QuerySnapshot>
 
-    @Before
-    fun setUp() {
-        firestore = mockk(relaxed = true)
-        val ingredient = Ingredient(
-            "Test Ingredient", 1, NutritionalInformation(
-                mutableListOf()
-            )
-        )// Create a real instance of Ingredient
-        val ingredients = mutableListOf(ingredient)
-        meal = mockk<Meal>(relaxed = true)
-        every { meal.mealID } returns 1
-        every { meal.occasion } returns MealOccasion.BREAKFAST // replace with the appropriate MealOccasion
-        every { meal.name } returns "Test Meal"
-        every { meal.mealTemp } returns 20.0
-        every { meal.nutritionalInformation } returns mockk(relaxed = true)
-//        every { meal.ingredients } returns ingredients
-        db = MealFirebaseRepository("test", firestore)
+  @Before
+  fun setUp() {
+    firestore = mockk(relaxed = true)
+    meal =
+        Meal(
+            mealID = 1,
+            name = "Test Meal",
+            occasion = MealOccasion.BREAKFAST, // replace with the appropriate MealOccasion
+            mealTemp = 20.0,
+            nutritionalInformation = NutritionalInformation(mutableListOf()),
+            ingredients = mutableListOf())
+    db = MealFirebaseRepository("test", firestore)
+  }
 
-        val documentReference = mockk<DocumentReference>(relaxed = true)
-        val collectionReference = mockk<CollectionReference> {
-            every { document(any()) } returns documentReference
-        }
+  @Test
+  fun storeMealSuccessfully() {
+    val result = db.storeMeal(meal)
+    result.continueWith { assertEquals(it.isSuccessful, true) }
+  }
 
-        task = mockk<Task<QuerySnapshot>>()
-        every {
-            firestore.collection(any()).document(any()).collection(any()).get()
-        } returns task
+  // Then in your test, you can mock the TaskWrapper class instead of the Task class
+  @Test
+  fun getMealSuccessfully() {
+    // Mock the DocumentSnapshot
+    val documentSnapshot = mockk<DocumentSnapshot>()
+    every { documentSnapshot.toObject(Meal::class.java) } returns meal
 
-        every { firestore.collection(any()) } returns collectionReference
+    // Mock the Task
+    val task = mockk<Task<DocumentSnapshot>>()
+    every { task.isSuccessful } returns true
+    every { task.result } returns documentSnapshot
+
+    // Mock the Firestore call
+    every {
+      firestore.collection(any()).document(any()).collection(any()).document(any()).get()
+    } returns task
+
+    // Call the method under test
+    val result = db.getMeal("0")
+
+    // Assert that the result is as expected
+    result.continueWith {
+      assertEquals(it.isSuccessful, true)
+      assertEquals(it.result, meal)
     }
+  }
 
-    @Test
-    fun storeMealSuccessfully() {
-        val result = db.storeMeal(meal)
-        result.continueWith {
-            assertEquals(it.isSuccessful, true)
-        }
+  @Test
+  fun getAllMealsSuccessfully() {
+    // Mock the DocumentSnapshot
+    val documentSnapshot = mockk<DocumentSnapshot>()
+    every { documentSnapshot.toObject(Meal::class.java) } returns meal
+
+    // Mock the QuerySnapshot
+    val querySnapshot = mockk<QuerySnapshot>()
+    every { querySnapshot.documents } returns mutableListOf(documentSnapshot)
+
+    // Mock the Task
+    val mockedTask = Tasks.forResult(querySnapshot)
+
+    // Mock the Firestore call
+    every { firestore.collection(any()).document(any()).collection(any()).get() } returns mockedTask
+
+    // Call the method under test
+    val result = db.getAllMeals()
+
+    // Assert that the result is as expected
+    result.continueWith {
+      assertEquals(it.isSuccessful, true)
+      assertEquals(it.result, listOf(meal))
     }
+  }
 
+  @Test
+  fun getMealThrowsExceptionWhenFailed() {
+    every {
+      firestore.collection(any()).document(any()).collection(any()).document(any()).get()
+    } returns Tasks.forException(Exception("Failed to fetch meals"))
 
-    // Then in your test, you can mock the TaskWrapper class instead of the Task class
-    @Test
-    fun getMealSuccessfully() {
-        // Mock the DocumentSnapshot
-        val documentSnapshot = mockk<DocumentSnapshot>()
-        every { documentSnapshot.toObject(Meal::class.java) } returns meal
+    val task = db.getMeal("0")
 
-        // Mock the Task
-        val task = mockk<Task<DocumentSnapshot>>()
-        every { task.isSuccessful } returns true
-        every { task.result } returns documentSnapshot
-
-        // Mock the Firestore call
-        every {
-            firestore.collection(any()).document(any()).collection(any()).document(any()).get()
-        } returns task
-
-        // Call the method under test
-        val result = db.getMeal("0")
-
-        // Assert that the result is as expected
-        result.continueWith {
-            assertEquals(it.isSuccessful, true)
-            assertEquals(it.result, meal)
-        }
+    task.continueWith {
+      assertEquals(it.isSuccessful, false)
+      assertEquals(it.exception?.message, "Failed to fetch meals")
     }
-
-    @Test
-    fun getAllMealsSuccessfully() {
-        val querySnapshot = mockk<QuerySnapshot>()
-        every { querySnapshot.documents } returns mutableListOf(mockk<DocumentSnapshot> {
-            every { toObject(Meal::class.java) } returns meal
-        })
-
-        task = mockk<Task<QuerySnapshot>> {
-            every { isSuccessful } returns true
-            every { result } returns querySnapshot
-        }
-        every {
-            firestore.collection(any()).document(any()).collection(any()).get()
-        } returns task
-
-        val result = db.getAllMeals()
-
-        result.continueWith {
-            assertEquals(it.isSuccessful, true)
-//            assertEquals(it.result?.documents?.map { it.toObject(Meal::class.java) }, listOf(meal))
-        }
-    }
-
-    @Test
-    fun getMealThrowsExceptionWhenFailed() {
-        every {
-            firestore.collection(any()).document(any()).collection(any()).document(any()).get()
-        } returns Tasks.forException(Exception("Failed to fetch meals"))
-
-        val task = db.getMeal("0")
-
-        task.continueWith {
-            assertEquals(it.isSuccessful, false)
-            assertEquals(it.exception?.message, "Failed to fetch meals")
-        }
-
-    }
-
+  }
 }
--- app/src/main/java/com/github/se/polyfit/data/remote/firebase/MealFirebaseRepository.kt
+++ app/src/main/java/com/github/se/polyfit/data/remote/firebase/MealFirebaseRepository.kt
@@ -1,5 +1,6 @@
 package com.github.se.polyfit.data.remote.firebase
 
+import android.util.Log
 import com.github.se.polyfit.model.meal.Meal
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentReference
@@ -10,36 +11,55 @@ class MealFirebaseRepository(
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 ) {
 
-    private val mealCollection = db.collection("users").document(userId).collection("meals")
+  private val mealCollection = db.collection("users").document(userId).collection("meals")
 
-    fun storeMeal(meal: Meal): Task<DocumentReference> {
-        val mealData = Meal.serialize(meal)
-        return mealCollection.add(mealData)
+  fun storeMeal(meal: Meal): Task<DocumentReference> {
+    val mealData = Meal.serialize(meal)
+    return mealCollection.add(mealData)
+  }
+
+  // maybe ok for now, but in the future will need to find a better way to
+  // distinguish between meals
+  fun getMeal(mealId: String): Task<Meal?> {
+    return getAllMeals().continueWith { task ->
+      Log.d("MealFirebaseRepository", "getMeal: $mealId")
+      if (task.isSuccessful) {
+
+        task.result?.find { it.mealID.toString() == mealId }
+      } else {
+        throw Exception("Failed to fetch meals : " + task.exception?.message)
+      }
     }
+  }
 
-    // maybe ok for now, but in the future will need to find a better way to
-    // distinguish between meals
-    fun getMeal(mealId: String): Task<Meal?> {
-        return getAllMeals().continueWith { task ->
-            if (task.isSuccessful) {
+  fun getAllMeals(): Task<List<Meal>> {
+    val task = mealCollection.get()
 
-                task.result?.find { it.mealID.toString() == mealId }
-            } else {
-                throw Exception("Failed to fetch meals : " + task.exception?.message)
-            }
+    // Add logging and failure handling right away
+    task
+        .addOnSuccessListener { documents ->
+          Log.d("MealFirebaseRepository", "Fetched ${documents.size()} meals successfully.")
         }
-    }
-
-    fun getAllMeals(): Task<List<Meal>> {
-        return mealCollection.get().continueWith { task ->
-            if (task.isSuccessful) {
-
-                task.result?.documents?.mapNotNull { document ->
-                    document.data?.let { Meal.deserialize(it) }
-                } ?: listOf()
-            } else {
-                throw Exception("Failed to fetch meals")
-            }
+        .addOnFailureListener { exception ->
+          Log.e("MealFirebaseRepository", "Failed to get meals", exception)
+          throw exception
         }
+
+    return task.continueWith { task ->
+      if (task.isSuccessful && task.result != null) {
+        try {
+          // Convert documents to Meal objects
+          task.result.documents.mapNotNull { document ->
+            document.data?.let { Meal.deserialize(it) }
+          }
+        } catch (e: Exception) {
+          Log.e("MealFirebaseRepository", "Error processing meal documents", e)
+          listOf<Meal>()
+        }
+      } else {
+        Log.e("MealFirebaseRepository", "Failed to fetch meals: ${task.exception?.message}")
+        throw Exception("Failed to fetch meals: ${task.exception?.message}")
+      }
     }
+  }
 }
