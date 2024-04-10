f028e1b5602eaa7ef2681dec8c11f2c18427b702
Neb <56965385+Neb1010@users.noreply.github.com>
2024-04-10 23:09:10 +0200

    fun + test

---
+++ app/src/androidTest/java/com/github/se/polyfit/ui/utils/UiExtrasDevTest.kt
@@ -0,0 +1,54 @@
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.se.polyfit.ui.utils.FunctionalityNotAvailablePopup
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FunctionalityNotAvailablePopupTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Test
+  fun simpleComposable_showsPopupWhenButtonClicked() {
+    composeTestRule.setContent { SimpleComposable() }
+
+    composeTestRule.onNodeWithText("Functionality not available \uD83D\uDE48").assertDoesNotExist()
+
+    composeTestRule.onNodeWithText("Button Text").performClick()
+
+    composeTestRule.onNodeWithText("Functionality not available \uD83D\uDE48").assertExists()
+  }
+
+  @Test
+  fun simpleComposable_dismissesPopupWhenCloseClicked() {
+    composeTestRule.setContent { SimpleComposable() }
+
+    composeTestRule.onNodeWithText("Button Text").performClick()
+
+    composeTestRule.onNodeWithText("CLOSE").performClick()
+
+    composeTestRule.onNodeWithText("Functionality not available \uD83D\uDE48").assertDoesNotExist()
+  }
+}
+
+@Composable
+fun SimpleComposable() {
+  var notAvailablePopup by remember { mutableStateOf(false) }
+  if (notAvailablePopup) {
+    FunctionalityNotAvailablePopup { notAvailablePopup = false }
+  }
+  Button(onClick = { notAvailablePopup = true }, content = { Text("Button Text") })
+  //
+
+}
