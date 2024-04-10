d53680470f7f730d6b8ee9dc720e8bd7c4ccc85e
medwardson <mnaidooe@uwaterloo.ca>
2024-04-11 01:28:51 +0200

    wip: testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -156,6 +156,7 @@ android {
         androidTestImplementation("androidx.test.ext:junit:1.1.5")
         androidTestImplementation("androidx.test:runner:1.5.2")
         androidTestImplementation("androidx.test:rules:1.5.0")
+
         androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
         androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
         androidTestImplementation("androidx.compose.ui:ui-test-junit4")
