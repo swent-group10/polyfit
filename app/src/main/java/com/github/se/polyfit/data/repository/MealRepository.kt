3ecd0cdafb0fe6c6a68995941450cb62af322f0f
RandomUsername1315 <charles.rousset@epfl.ch>
2024-04-10 08:22:55 +0200

    Basic Interface

--- app/src/main/java/com/github/se/polyfit/data/repository/MealRepository.kt
+++ app/src/main/java/com/github/se/polyfit/data/repository/MealRepository.kt
@@ -2,16 +2,14 @@ package com.github.se.polyfit.data.repository
 
 import com.github.se.polyfit.data.remote.firebase.MealFirebaseRepository
 import com.github.se.polyfit.model.meal.Meal
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 
 /**
  * Repository for meals responsible for handling the firebase repository and the local database
  * (future implementation)
  */
 class MealRepository(
-    private val mealFirebaseRepository: MealFirebaseRepository,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.Default
+    private val userID: String,
+    private val mealFirebaseRepository: MealFirebaseRepository = MealFirebaseRepository(userID),
 ) {
 
     fun storeMeal(meal: Meal) = mealFirebaseRepository.storeMeal(meal)
--- app/src/main/java/com/github/se/polyfit/viewmodel/meal/MealViewModel.kt
+++ app/src/main/java/com/github/se/polyfit/viewmodel/meal/MealViewModel.kt
@@ -1,3 +1,36 @@
 package com.github.se.polyfit.viewmodel.meal
 
-class MealViewModel
+import com.github.se.polyfit.data.repository.MealRepository
+import com.github.se.polyfit.model.ingredient.Ingredient
+
+class MealViewModel(userID: String) {
+    // after friday use hilt dependency injection to make code cleaner, for now
+    // i gues this is ok
+    private val mealRepo: MealRepository = MealRepository(userID)
+
+    fun getAllMeals() {
+    }
+
+    fun getIngredientsFromImage(image: String) {}
+
+    fun setIngredients(mealID: Long, ingredient: Ingredient) {}
+
+    fun getAllIngredients() {
+
+    }
+
+
+}
+
+
+/*
+features requested by mesha : Sweet!
+
+
+getIngredients (for initial display, i guess either a list of all ingredients or if we support it, one for confirmed and one for potential ingredients)
+addIngredient (add from potential ingredient list, or from the add button)
+removeIngredient (for when expanding the ingredient is supported)
+
+
+
+ */
