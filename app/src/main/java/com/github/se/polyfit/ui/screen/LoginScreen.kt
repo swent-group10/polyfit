aead12221f3157d45b148548fd54d39d9e925288
Antoine Cornaz <antomax33.jeux@gmail.com>
2024-04-12 15:20:23 +0200

    cleaning hilt useless and remove hard-coded value

--- app/src/main/java/com/github/se/polyfit/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/github/se/polyfit/ui/screen/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.github.se.polyfit.ui.screen
 
+import android.content.Context
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.Image
@@ -19,8 +20,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -28,34 +31,36 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.core.content.ContextCompat.getString
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.github.se.polyfit.R
 import com.github.se.polyfit.ui.compose.Title
 import com.github.se.polyfit.ui.theme.PrimaryPurple
-import com.github.se.polyfit.ui.viewModel.LoginViewModel
+import com.github.se.polyfit.ui.utils.AuthenticationCloud
 
 @Composable
 fun LoginScreen(goTo: () -> Unit) {
   // Create an instance of the Authentication class
-
-  val viewModel: LoginViewModel = hiltViewModel()
+  val context = LocalContext.current
+  val authenticationCloud = AuthenticationCloud(context)
 
   val signInLauncher =
       rememberLauncherForActivityResult(contract = FirebaseAuthUIActivityResultContract()) { res ->
-        viewModel.onSignInResult(res) { if (it) goTo() else Log.d("LoginScreen", "Sign in failed") }
+        authenticationCloud.onSignInResult(res) { if (it) goTo() else Log.d("LoginScreen", "Sign in failed") }
       }
 
   // Set the signInLauncher in the Authentication class
-  viewModel.setSignInLauncher(signInLauncher)
+  authenticationCloud.setSignInLauncher(signInLauncher)
 
   // This function starts the sign-in process
   fun createSignInIntent() {
-    viewModel.signIn()
+    authenticationCloud.signIn()
   }
 
   Surface(
-      modifier = Modifier.fillMaxSize().testTag("LoginScreen"),
+      modifier = Modifier
+        .fillMaxSize()
+        .testTag("LoginScreen"),
       color = MaterialTheme.colorScheme.background) {
         Column(
             verticalArrangement = Arrangement.Center,
@@ -74,19 +79,23 @@ fun LoginScreen(goTo: () -> Unit) {
               Spacer(Modifier.weight(0.03f))
 
               Text(
-                  termText, textAlign = TextAlign.Center, modifier = Modifier.testTag("LoginTerms"))
+                  getConditionString(context = context), textAlign = TextAlign.Center, modifier = Modifier.testTag("LoginTerms"))
 
               Spacer(Modifier.weight(0.2f))
             }
       }
 }
 
-val termText = buildAnnotatedString {
-  append("By clicking continue, you agree to our \n")
-  withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) { append("Terms of Service") }
-  append(" and ")
-
-  withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) { append("Privacy Policy") }
+@Composable
+fun getConditionString(context: Context): AnnotatedString{
+  return buildAnnotatedString {
+    append(getString(context, R.string.condition1))
+    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+      append(getString(context, R.string.condition2)) }
+    append(getString(context, R.string.condition3))
+    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+      append(getString(context, R.string.condition4)) }
+  }
 }
 
 @Composable
@@ -99,13 +108,15 @@ fun SignInButton(onClick: () -> Unit) {
           ButtonDefaults.buttonColors(
               contentColor = MaterialTheme.colorScheme.onPrimary, containerColor = PrimaryPurple),
       modifier = Modifier.testTag("LoginButton")) {
-        val imageModifierGoogle = Modifier.size(24.dp).absoluteOffset(x = (-13).dp, y = 0.dp)
+        val imageModifierGoogle = Modifier
+          .size(24.dp)
+          .absoluteOffset(x = (-13).dp, y = 0.dp)
 
         Image(
             painter = painterResource(id = R.drawable.google_logo),
             contentDescription = "Google_logo",
             contentScale = ContentScale.Fit,
             modifier = imageModifierGoogle)
-        Text(text = "Sign in with Google")
+        Text(text = getString(LocalContext.current, R.string.signGoogle))
       }
 }
