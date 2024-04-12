c79f78e7ef9c8b08904d7a562669e2126a7f7b2f
RandomUsername1315 <charles.rousset@epfl.ch>
2024-04-12 19:46:29 +0200

    kfmt

--- app/src/main/java/com/github/se/polyfit/ui/components/IngredientList.kt
+++ app/src/main/java/com/github/se/polyfit/ui/components/IngredientList.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
