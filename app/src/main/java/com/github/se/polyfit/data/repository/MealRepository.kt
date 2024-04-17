5d4614e90583bca59d12a144fb80cb266770e3df
RandomUsername1315 <charles.rousset@epfl.ch>
2024-04-17 10:19:28 +0200

    Add network connectivity checker

--- app/src/main/java/com/github/se/polyfit/data/repository/MealRepository.kt
+++ app/src/main/java/com/github/se/polyfit/data/repository/MealRepository.kt
@@ -1,5 +1,7 @@
 package com.github.se.polyfit.data.repository
 
+import android.content.Context
+import android.net.ConnectivityManager
 import com.github.se.polyfit.data.remote.firebase.MealFirebaseRepository
 import com.github.se.polyfit.model.meal.Meal
 import kotlinx.coroutines.CoroutineDispatcher
@@ -7,14 +9,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
-/**
- * Repository for meals responsible for handling the firebase repository and the local database
- * (future implementation)
- */
+/** Repository for meals responsible for handling the firebase repository and the local database */
 class MealRepository(
+    private val context: Context,
     private val mealFirebaseRepository: MealFirebaseRepository,
     private val dispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
+  private val checkConnecivity = connectivityChecker(context)
 
   suspend fun storeMeal(meal: Meal) =
       withContext(dispatcher) { mealFirebaseRepository.storeMeal(meal).await() }
@@ -27,4 +28,14 @@ class MealRepository(
 
   suspend fun deleteMeal(firebaseID: String) =
       withContext(dispatcher) { mealFirebaseRepository.deleteMeal(firebaseID).await() }
+}
+
+class connectivityChecker(private val context: Context) {
+  fun checkConnection(): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val currentNetwork = connectivityManager.activeNetwork
+
+    return currentNetwork == null
+  }
 }
