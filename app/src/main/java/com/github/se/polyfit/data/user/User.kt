7c000498336005fe985d02d5c3792c736a143011
RandomUsername1315 <charles.rousset@epfl.ch>
2024-04-21 12:25:53 +0200

    Add back functionality removed from user

--- app/src/main/java/com/github/se/polyfit/data/user/User.kt
+++ app/src/main/java/com/github/se/polyfit/data/user/User.kt
@@ -1,6 +1,7 @@
 package com.github.se.polyfit.model.data
 
 import android.net.Uri
+import android.util.Log
 
 data class User(
     var id: String = "",
@@ -9,4 +10,65 @@ data class User(
     var givenName: String? = null,
     var email: String = "",
     var photoURL: Uri? = null
-)
+) {
+
+  fun signOut() {
+    id = ""
+    displayName = null
+    familyName = null
+    givenName = null
+    email = ""
+    photoURL = null
+  }
+
+  fun isSignedIn(): Boolean {
+    return id.isNotEmpty()
+  }
+
+  fun serialize(): Map<String, Any> {
+    return serialize(this)
+  }
+
+  fun update(
+      id: String = this.id,
+      displayName: String? = this.displayName,
+      familyName: String? = this.familyName,
+      givenName: String? = this.givenName,
+      email: String = this.email,
+      photoURL: Uri? = this.photoURL
+  ) {
+    this.id = id
+    this.displayName = displayName
+    this.familyName = familyName
+    this.givenName = givenName
+    this.email = email
+    this.photoURL = photoURL
+  }
+
+  companion object {
+    fun serialize(user: User): Map<String, Any> {
+      return mapOf(
+          "id" to user.id,
+          "displayName" to user.displayName!!,
+          "familyName" to user.familyName!!,
+          "givenName" to user.givenName!!,
+          "email" to user.email,
+          "photoURL" to user.photoURL.toString())
+    }
+
+    fun deserialize(map: Map<String, Any>): User {
+      return try {
+        User(
+            id = map["id"] as String,
+            displayName = map["displayName"] as String,
+            familyName = map["familyName"] as String,
+            givenName = map["givenName"] as String,
+            email = map["email"] as String,
+            photoURL = Uri.parse(map["photoURL"] as String))
+      } catch (e: Exception) {
+        Log.e("User", "Error deserializing user", e)
+        throw IllegalArgumentException("Error deserializing user : $e")
+      }
+    }
+  }
+}
--- app/src/test/java/com/github/se/polyfit/data/user/UserTest.kt
+++ app/src/test/java/com/github/se/polyfit/data/user/UserTest.kt
@@ -1,11 +1,26 @@
 package com.github.se.polyfit.data.user
 
+import android.net.Uri
+import android.util.Log
 import com.github.se.polyfit.model.data.User
+import io.mockk.every
+import io.mockk.mockkStatic
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertNull
+import org.junit.Before
 
 class UserTest {
+
+  @Before
+  fun setup() {
+    mockkStatic(Log::class)
+    every { Log.e(any(), any(), any()) } returns 0
+
+    mockkStatic(Uri::class)
+    every { Uri.parse(any()) } returns Uri.EMPTY
+  }
 
   @Test
   fun `User creation with null email`() {
@@ -26,5 +41,92 @@ class UserTest {
     assertEquals("User", user.givenName)
     assertEquals("invalid email", user.email)
     assertNull(user.photoURL)
+  }
+
+  @Test
+  fun `User reset`() {
+    val user = User("1", "Test User", "Test", "User", " invalid email", null)
+    user.signOut()
+    assertEquals("", user.id)
+    assertNull(user.displayName)
+    assertNull(user.familyName)
+  }
+
+  @Test
+  fun `User is signed in`() {
+    val user = User("1", "Test User", "Test", "User", " invalid email", null)
+    assertEquals(true, user.isSignedIn())
+  }
+
+  @Test
+  fun `User serialization`() {
+    val user = User("1", "Test User", "Test", "User", " invalid email", null)
+    val map = User.serialize(user)
+    assertEquals("1", map["id"])
+    assertEquals("Test User", map["displayName"])
+    assertEquals("Test", map["familyName"])
+    assertEquals("User", map["givenName"])
+    assertEquals(" invalid email", map["email"])
+    assertEquals("null", map["photoURL"])
+  }
+
+  @Test
+  fun `User serialization function`() {
+    val user = User("1", "Test User", "Test", "User", " invalid email", null)
+    val map = user.serialize()
+    assertEquals("1", map["id"])
+    assertEquals("Test User", map["displayName"])
+    assertEquals("Test", map["familyName"])
+    assertEquals("User", map["givenName"])
+    assertEquals(" invalid email", map["email"])
+    assertEquals("null", map["photoURL"])
+  }
+
+  @Test
+  fun `User deserialization`() {
+    val map =
+        mapOf(
+            "id" to "1",
+            "displayName" to "Test User",
+            "familyName" to "Test",
+            "givenName" to "User",
+            "email" to " invalid email",
+            "photoURL" to "null")
+    val user = User.deserialize(map)
+    assertEquals("1", user.id)
+    assertEquals("Test User", user.displayName)
+    assertEquals("Test", user.familyName)
+    assertEquals("User", user.givenName)
+    assertEquals(" invalid email", user.email)
+    assertNull(user.photoURL)
+  }
+
+  @Test
+  fun `Update user`() {
+    val user = User("1", "Test User", "Test", "User", " invalid email", null)
+    user.update("2", "Test User 2", "Test 2", "User 2", " invalid email 2", null)
+    assertEquals("2", user.id)
+    assertEquals("Test User 2", user.displayName)
+    assertEquals("Test 2", user.familyName)
+    assertEquals("User 2", user.givenName)
+    assertEquals(" invalid email 2", user.email)
+    assertNull(user.photoURL)
+
+    user.update(id = "3")
+    assertEquals("3", user.id)
+    user.update(displayName = "Test User 3")
+    assertEquals("Test User 3", user.displayName)
+  }
+
+  @Test
+  fun `User deserialization with invalid map`() {
+    val map =
+        mapOf(
+            "id" to "null",
+            "familyName" to "Test",
+            "givenName" to "User",
+            "email" to " invalid email",
+            "photoURL" to "null")
+    assertFailsWith<Exception> { User.deserialize(map) }
   }
 }
