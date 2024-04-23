50586191167a6ce22ce1c04841ebb7d8763dd0c9
RandomUsername1315 <charles.rousset@epfl.ch>
2024-04-23 14:59:27 +0200

    PR suggestions

--- app/src/main/java/com/github/se/polyfit/model/post/Post.kt
+++ app/src/main/java/com/github/se/polyfit/model/post/Post.kt
@@ -75,15 +75,3 @@ data class Post(
  * Location data class allows to store only the necessary information about the location without all
  * the other required information like in the android.location.Location class
  */
-data class Location(
-    val longitude: Double,
-    val latitude: Double,
-    val altitude: Double,
-    val name: String
-) {
-  companion object {
-    fun default(): Location {
-      return Location(0.0, 0.0, 0.0, "EPFL")
-    }
-  }
-}
