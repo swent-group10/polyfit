175b8b1f260afadb00c56d824e13782411be8846
Neb <56965385+Neb1010@users.noreply.github.com>
2024-04-25 23:30:44 +0200

    Push the post on the db and in the hash in rtdb, just need to write more tests for coverage

--- app/src/main/java/com/github/se/polyfit/data/remote/firebase/GeoQueryRepository.kt
+++ app/src/main/java/com/github/se/polyfit/data/remote/firebase/GeoQueryRepository.kt
@@ -1,5 +1,6 @@
 package com.github.se.polyfit.data.remote.firebase
 
+import android.util.Log
 import com.firebase.geofire.GeoFire
 import com.firebase.geofire.GeoLocation
 import com.firebase.geofire.GeoQueryEventListener
@@ -7,39 +8,44 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 
 class GeoQueryRepository(
-    rtdb: FirebaseDatabase = FirebaseDatabase.getInstance("your_realtime_database_url")
+    rtdb: FirebaseDatabase =
+        FirebaseDatabase.getInstance(
+            "https://polyfit-316e8-default-rtdb.europe-west1.firebasedatabase.app/")
 ) {
   private val geoFireRef = rtdb.getReference("posts_location")
   private val geoFire = GeoFire(geoFireRef)
 
   /**
-   * Initiates a query to find posts within a specified radius around a given center latitude and longitude.
-   * The results are provided asynchronously via a GeoQueryEventListener.
+   * Initiates a query to find posts within a specified radius around a given center latitude and
+   * longitude. The results are provided asynchronously via a GeoQueryEventListener.
    *
-   * This function sets up a GeoQuery to listen for events related to the specified geographical area.
-   * It reacts to posts that enter, exit, or move within this area.
+   * This function sets up a GeoQuery to listen for events related to the specified geographical
+   * area. It reacts to posts that enter, exit, or move within this area.
    *
-   * The nearby post IDs are accumulated and once the initial set of data is loaded (onGeoQueryReady),
-   * it triggers a fetch for the actual post data from Firestore.
+   * The nearby post IDs are accumulated and once the initial set of data is loaded
+   * (onGeoQueryReady), it triggers a fetch for the actual post data from Firestore.
    *
    * @param centerLatitude The latitude of the center point for the query.
    * @param centerLongitude The longitude of the center point for the query.
-   * @param radiusInKm The radius around the center point in kilometers within which to search for posts.
+   * @param radiusInKm The radius around the center point in kilometers within which to search for
+   *   posts.
    */
-
   fun queryNearbyPosts(centerLatitude: Double, centerLongitude: Double, radiusInKm: Double) {
     val center = GeoLocation(centerLatitude, centerLongitude)
     val query = geoFire.queryAtLocation(center, radiusInKm)
-
+    Log.d("GeoQuery", "Querying posts within $radiusInKm km of $center")
     query.addGeoQueryEventListener(
         object : GeoQueryEventListener {
           val nearbyKeys = mutableListOf<String>()
 
+
           override fun onKeyEntered(key: String, location: GeoLocation) {
+            Log.d("GeoQuery", "Key entered: $key")
             nearbyKeys.add(key)
           }
 
           override fun onKeyExited(key: String) {
+            Log.d("GeoQuery", "Key exited: $key")
 
             nearbyKeys.remove(key)
           }
@@ -51,6 +57,7 @@ class GeoQueryRepository(
 
           override fun onGeoQueryReady() {
 
+            Log.d("GeoQuery", "GeoQuery is ready. Collected keys: $nearbyKeys")
             PostFirebaseRepository().fetchPosts(nearbyKeys) { posts ->
               // Do something with the fetched posts
             }
@@ -58,7 +65,7 @@ class GeoQueryRepository(
 
           override fun onGeoQueryError(error: DatabaseError) {
 
-            println("There was an error with the geo query: ${error.message}")
+            Log.e("GeoQuery", "There was an error with the geo query: ${error.message}")
           }
         })
   }
