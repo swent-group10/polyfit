bf7520393234dd6818e6fd2db42649cdb773318b
RandomUsername1315 <charles.rousset@epfl.ch>
2024-04-26 15:25:58 +0200

    Successfully stores images

--- app/src/main/java/com/github/se/polyfit/data/remote/firebase/PostFirebaseRepository.kt
+++ app/src/main/java/com/github/se/polyfit/data/remote/firebase/PostFirebaseRepository.kt
@@ -1,22 +1,71 @@
 package com.github.se.polyfit.data.remote.firebase
 
+import android.graphics.Bitmap
 import android.util.Log
 import com.github.se.polyfit.model.post.Post
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.util.UUID
 
-class PostFirebaseRepository(db: FirebaseFirestore = FirebaseFirestore.getInstance()) {
-  private val postCollection = db.collection("posts")
+class PostFirebaseRepository(
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance(),
+    private val pictureDb: FirebaseStorage = FirebaseStorage.getInstance()
+) {
+    private val postCollection = db.collection("posts")
 
-  fun storePost(post: Post): Task<DocumentReference> {
-    return postCollection.add(post.serialize()).continueWithTask {
-      if (it.isSuccessful) {
-        return@continueWithTask it
-      } else {
-        Log.e("PostFirebaseRepository", "Failed to store post in the database : ${it.exception}")
-        throw Exception("Failed to store post in the database : ${it.exception}")
-      }
+    fun storePost(post: Post): Task<Void> {
+        val documentTask = postCollection.add(post.serialize())
+
+        return documentTask.continueWithTask { task ->
+            if (task.isSuccessful) {
+                val documentRef = task.result
+                if (post.listOfImages.isNotEmpty()) {
+                    val uploadTask = post.listOfImages.map {
+                        uploadImage(it, documentRef)
+                    }
+
+                    Tasks.whenAll(uploadTask)
+
+                } else {
+                    Tasks.forResult(null) // No images to upload, return null
+                }
+            } else {
+                Log.e(
+                    "PostFirebaseRepository", "Failed to store post in the database", task.exception
+                )
+                task.exception?.let { Tasks.forException(it) }
+            }
+
+        }
     }
-  }
+
+
+    private fun uploadImage(image: Bitmap, documentRef: DocumentReference): Task<Void> {
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val stream = ByteArrayInputStream(data)
+
+        val path = "${documentRef.path}/${UUID.randomUUID()}.jpg"
+        val refSource = pictureDb.getReference(path)
+        return refSource.putStream(stream).continueWithTask { uploadTask ->
+            if (uploadTask.isSuccessful) {
+                Log.d("PostFirebaseRepository", "Image uploaded successfully")
+                Tasks.forResult(null) // Return null to signify completion
+            } else {
+                Log.e(
+                    "PostFirebaseRepository", "Failed to upload image", uploadTask.exception
+                )
+                uploadTask.exception?.let { Tasks.forException(it) }
+            }
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/github/se/polyfit/model/post/Post.kt
+++ app/src/main/java/com/github/se/polyfit/model/post/Post.kt
@@ -1,15 +1,16 @@
 package com.github.se.polyfit.model.post
 
+import android.graphics.Bitmap
 import com.github.se.polyfit.model.meal.Meal
 import com.github.se.polyfit.model.nutritionalInformation.Nutrient
 import java.time.LocalDate
 
 interface UnmodifiablePost {
-  val userId: String
-  val description: String
-  val location: Location
-  val meal: Meal
-  val createdAt: LocalDate
+    val userId: String
+    val description: String
+    val location: Location
+    val meal: Meal
+    val createdAt: LocalDate
 }
 
 data class Post(
@@ -17,64 +18,66 @@ data class Post(
     override var description: String,
     override var location: Location,
     override var meal: Meal,
-    override var createdAt: LocalDate
+    override var createdAt: LocalDate,
+    var listOfImages: List<Bitmap> = emptyList()
 ) : UnmodifiablePost {
-  override fun toString(): String {
-    return "The post from the user ${userId} with the following description ${description}" +
-        " and the following location ${location}" +
-        " for following meal ${meal}"
-  }
-
-  fun getCarbs(): Nutrient? {
-    return meal.nutritionalInformation.getNutrient("carb")
-  }
-
-  fun getFat(): Nutrient? {
-    return meal.nutritionalInformation.getNutrient("fat")
-  }
-
-  fun getProtein(): Nutrient? {
-    return meal.nutritionalInformation.getNutrient("protein")
-  }
-
-  fun serialize(): Map<String, Any> {
-    return serialize(this)
-  }
-
-  // Useful function for the ui
-  fun getIngredientCalories(): List<Pair<String, Nutrient>> {
-    return meal.ingredients.mapNotNull { ingredient ->
-      val nutrient =
-          ingredient.nutritionalInformation.nutrients.firstOrNull { it.nutrientType == "calories" }
-      if (nutrient != null) Pair(ingredient.name, nutrient) else null
-    }
-  }
-
-  // Really useful function for the ui
-  fun getIngredientWeight(): List<Pair<String, Nutrient>> {
-    return meal.ingredients.mapNotNull { ingredient ->
-      Pair(ingredient.name, Nutrient("totalWeight", ingredient.amount, ingredient.unit))
-    }
-  }
-
-  companion object {
-    fun serialize(data: Post): Map<String, Any> {
-      return mutableMapOf<String, Any>().apply {
-        this["userId"] = data.userId
-        this["description"] = data.description
-        this["location"] = data.location
-        this["meal"] = data.meal.serialize()
-        this["createdAt"] = data.createdAt
-      }
+    override fun toString(): String {
+        return "The post from the user ${userId} with the following description ${description}" +
+                " and the following location ${location}" +
+                " for following meal ${meal}"
     }
 
-    fun default(): Post {
-      return Post(
-          "testId",
-          "Description",
-          Location(0.0, 0.0, 10.0, "EPFL"),
-          Meal.default(),
-          LocalDate.now())
+    fun getCarbs(): Nutrient? {
+        return meal.nutritionalInformation.getNutrient("carb")
     }
-  }
+
+    fun getFat(): Nutrient? {
+        return meal.nutritionalInformation.getNutrient("fat")
+    }
+
+    fun getProtein(): Nutrient? {
+        return meal.nutritionalInformation.getNutrient("protein")
+    }
+
+    fun serialize(): Map<String, Any> {
+        return serialize(this)
+    }
+
+    // Useful function for the ui
+    fun getIngredientCalories(): List<Pair<String, Nutrient>> {
+        return meal.ingredients.mapNotNull { ingredient ->
+            val nutrient =
+                ingredient.nutritionalInformation.nutrients.firstOrNull { it.nutrientType == "calories" }
+            if (nutrient != null) Pair(ingredient.name, nutrient) else null
+        }
+    }
+
+    // Really useful function for the ui
+    fun getIngredientWeight(): List<Pair<String, Nutrient>> {
+        return meal.ingredients.mapNotNull { ingredient ->
+            Pair(ingredient.name, Nutrient("totalWeight", ingredient.amount, ingredient.unit))
+        }
+    }
+
+    companion object {
+        fun serialize(data: Post): Map<String, Any> {
+            return mutableMapOf<String, Any>().apply {
+                this["userId"] = data.userId
+                this["description"] = data.description
+                this["location"] = data.location
+                this["meal"] = data.meal.serialize()
+                this["createdAt"] = data.createdAt
+            }
+        }
+
+        fun default(): Post {
+            return Post(
+                "testId",
+                "Description",
+                Location(0.0, 0.0, 10.0, "EPFL"),
+                Meal.default(),
+                LocalDate.now()
+            )
+        }
+    }
 }
