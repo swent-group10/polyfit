ea47dab9f31636abf2b118ba93866f6845a0ec03
Antoine Cornaz <antomax33.jeux@gmail.com>
2024-04-30 11:47:27 +0200

    put again authModule

---
+++ app/src/main/java/com/github/se/polyfit/ui/AuthModule.kt
@@ -0,0 +1,20 @@
+package com.github.se.polyfit.ui.hiltModule
+
+import android.content.Context
+import com.github.se.polyfit.model.data.User
+import com.github.se.polyfit.ui.utils.Authentication
+import com.github.se.polyfit.ui.utils.AuthenticationCloud
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object AuthModule {
+  @Provides
+  fun provideAuthentication(@ApplicationContext context: Context, user: User): Authentication {
+    return AuthenticationCloud(context, user)
+  }
+}
