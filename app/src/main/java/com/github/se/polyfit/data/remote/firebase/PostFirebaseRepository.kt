de8420bd77d59e6eaa0575e6589a439c9a6a5cbd
Charles Rousset <100274930+RandomUsername1315@users.noreply.github.com>
2024-04-30 14:54:52 +0200

    Stores Images associated with Posts on the storage firebase (#172)
    * Successfully stores images
    
    * Implementing Tests
    
    * New implementation
    
    Really difficult to mock task so switched to a suspend function implementation
    
    * Update Test
    
    * Update app/src/main/java/com/github/se/polyfit/viewmodel/post/CreatePostViewModel.kt

--- app/src/main/java/com/github/se/polyfit/data/remote/firebase/PostFirebaseRepository.kt
+++ app/src/main/java/com/github/se/polyfit/data/remote/firebase/PostFirebaseRepository.kt
@@ -1,22 +1,52 @@
 package com.github.se.polyfit.data.remote.firebase
 
+import android.graphics.Bitmap
 import android.util.Log
 import com.github.se.polyfit.model.post.Post
-import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.util.UUID
+import kotlinx.coroutines.tasks.await
 
-class PostFirebaseRepository(db: FirebaseFirestore = FirebaseFirestore.getInstance()) {
+class PostFirebaseRepository(
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance(),
+    private val pictureDb: FirebaseStorage = FirebaseStorage.getInstance()
+) {
   private val postCollection = db.collection("posts")
 
-  fun storePost(post: Post): Task<DocumentReference> {
-    return postCollection.add(post.serialize()).continueWithTask {
-      if (it.isSuccessful) {
-        return@continueWithTask it
-      } else {
-        Log.e("PostFirebaseRepository", "Failed to store post in the database : ${it.exception}")
-        throw Exception("Failed to store post in the database : ${it.exception}")
+  suspend fun storePost(post: Post): List<StorageReference> {
+    try {
+      val documentRef = postCollection.add(post.serialize()).await()
+      var listTaskSnapshot: List<StorageReference> = listOf()
+      if (post.listOfImages.isNotEmpty()) {
+        listTaskSnapshot = post.listOfImages.map { uploadImage(it, documentRef) }
       }
+      return listTaskSnapshot
+    } catch (e: Exception) {
+      Log.e("PostFirebaseRepository", "Failed to store post in the database", e)
+      throw Exception("Error uploading images : ${e.message}", e)
+    }
+  }
+
+  private suspend fun uploadImage(image: Bitmap, documentRef: DocumentReference): StorageReference {
+    val baos = ByteArrayOutputStream()
+    image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+    val data = baos.toByteArray()
+    val stream = ByteArrayInputStream(data)
+
+    val path = "${documentRef.path}/${UUID.randomUUID()}.jpg"
+    val refSource = pictureDb.getReference(path)
+
+    try {
+      val uploadTask = refSource.putStream(stream)
+      return uploadTask.await().storage
+    } catch (e: Exception) {
+      Log.e("PostFirebaseRepository", "Failed to upload image", e)
+      throw Exception("Error uploading images : ${e.message}", e)
     }
   }
 }
