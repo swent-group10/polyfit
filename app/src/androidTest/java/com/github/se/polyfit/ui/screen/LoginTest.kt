ab33bb3b6944389268c9013d59000a1c699f776a
Neb <56965385+Neb1010@users.noreply.github.com>
2024-05-01 21:05:28 +0200

    ktfmt

--- app/src/androidTest/java/com/github/se/polyfit/ui/screen/LoginTest.kt
+++ app/src/androidTest/java/com/github/se/polyfit/ui/screen/LoginTest.kt
@@ -7,7 +7,6 @@ import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.polyfit.MainActivity
-import com.github.se.polyfit.ui.utils.Authentication
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import kotlinx.coroutines.delay
