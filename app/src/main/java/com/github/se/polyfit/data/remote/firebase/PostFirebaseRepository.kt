0ae424e113055e984bc36f191618aa4f4cb1f57f
Neb <56965385+Neb1010@users.noreply.github.com>
2024-05-03 19:22:08 +0200

    Feature/backend/geofirewithpicture (#206)
    * Geoquery
    
    * deserialize post
    
    * deserialize location
    
    * Geoquery test
    
    * post test
    
    * post geofire
    
    * gradle
    
    * geofire
    
    * +
    
    * improvement
    
    * starting the tests, misère...
    
    * change test
    
    * buggy test
    
    * now the final boss.....the CI
    
    * ktfmt
    
    * we don't care bout these tests
    
    * pray the lord CI
    
    * ktfmt
    
    * remove Log.d
    
    * dependecies
    
    * build.gradle
    
    * desperate
    
    * disable my test to see if i pass the ci
    
    * I'm lost
    
    * ktfmt
    
    * change to follow pr review recommendation
    
    ---------
    
    Co-authored-by: Charles Rousset <100274930+RandomUsername1315@users.noreply.github.com>
    Co-authored-by: antomax33 <77206515+antomax33@users.noreply.github.com>

--- app/src/main/java/com/github/se/polyfit/data/remote/firebase/PostFirebaseRepository.kt
+++ app/src/main/java/com/github/se/polyfit/data/remote/firebase/PostFirebaseRepository.kt
@@ -1,19 +1,39 @@
 package com.github.se.polyfit.data.remote.firebase
 
 import android.util.Log
+import com.firebase.geofire.GeoFire
+import com.firebase.geofire.GeoLocation
+import com.firebase.geofire.GeoQueryEventListener
 import com.github.se.polyfit.model.post.Post
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class PostFirebaseRepository(
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance(),
-    private val pictureDb: FirebaseStorage = FirebaseStorage.getInstance()
+    private val pictureDb: FirebaseStorage = FirebaseStorage.getInstance(),
+    private val rtdb: FirebaseDatabase =
+        FirebaseDatabase.getInstance(
+            "https://polyfit-316e8-default-rtdb.europe-west1.firebasedatabase.app/")
 ) {
   private val postCollection = db.collection("posts")
+  private val geoFireRef = rtdb.getReference("posts_location")
+  private val geoFire = GeoFire(geoFireRef)
 
   suspend fun storePost(post: Post): DocumentReference? {
     try {
@@ -40,5 +60,111 @@ class PostFirebaseRepository(
       throw Exception("Error getting posts : ${e.message}", e)
     }
     emit(posts)
+  }
+
+  /**
+   * Initiates a query to find posts within a specified radius around a given center latitude and
+   * longitude. The results are provided asynchronously via a GeoQueryEventListener.
+   *
+   * This function sets up a GeoQuery to listen for events related to the specified geographical
+   * area. It reacts to posts that enter, exit, or move within this area.
+   *
+   * The nearby post IDs are accumulated and once the initial set of data is loaded
+   * (onGeoQueryReady), it triggers a fetch for the actual post data from Firestore.
+   *
+   * @param centerLatitude The latitude of the center point for the query.
+   * @param centerLongitude The longitude of the center point for the query.
+   * @param radiusInKm The radius around the center point in kilometers within which to search for
+   *   posts.
+   */
+  fun queryNearbyPosts(
+      centerLatitude: Double,
+      centerLongitude: Double,
+      radiusInKm: Double,
+      geoFire: GeoFire = GeoFire(geoFireRef)
+  ) {
+    val center = GeoLocation(centerLatitude, centerLongitude)
+    val query = geoFire.queryAtLocation(center, radiusInKm)
+
+    query.addGeoQueryEventListener(
+        object : GeoQueryEventListener {
+          val nearbyKeys = mutableListOf<String>()
+
+          override fun onKeyEntered(key: String, location: GeoLocation) {
+            nearbyKeys.add(key)
+          }
+
+          override fun onKeyExited(key: String) {
+            nearbyKeys.remove(key)
+          }
+
+          override fun onKeyMoved(key: String, location: GeoLocation) {
+            // Do nothing but need to be there
+          }
+
+          override fun onGeoQueryReady() {
+            PostFirebaseRepository().fetchPostsAndImages(nearbyKeys) { posts ->
+              // Do something with the fetched posts
+            }
+          }
+
+          override fun onGeoQueryError(error: DatabaseError) {
+            Log.e("GeoQuery", "There was an error with the geo query: ${error.message}")
+          }
+        })
+  }
+
+  fun fetchPostsAndImages(
+      keys: List<String>,
+      postCollection: CollectionReference = this.postCollection,
+      completion: (List<Post>) -> Unit
+  ) {
+    val posts = mutableListOf<Post>()
+    val batchSize = 10
+    val batches = keys.chunked(batchSize)
+    var completedBatches = 0
+
+    // Use a batch operation to fetch all posts
+    batches.forEach { batch ->
+      postCollection
+          .whereIn(FieldPath.documentId(), batch)
+          .get()
+          .addOnSuccessListener { querySnapshot ->
+            CoroutineScope(Dispatchers.Default).launch {
+              val tempPosts = mutableListOf<Post>()
+              val deferredImages = mutableListOf<Deferred<Unit>>()
+
+              querySnapshot.documents.forEach { document ->
+                document.data?.let {
+                  Post.deserialize(it)?.also { post ->
+                    deferredImages.add(
+                        async { post.listOfURLs = fetchImageReferencesForPost(document.id) })
+                    tempPosts.add(post)
+                  }
+                }
+              }
+              deferredImages.awaitAll()
+
+              synchronized(posts) {
+                posts.addAll(tempPosts)
+                completedBatches++
+                if (completedBatches == batches.size) {
+                  completion(posts)
+                }
+              }
+            }
+          }
+          .addOnFailureListener {
+            Log.e("PostFirebaseRepository", "Failed to fetch posts: ${it.message}")
+          }
+    }
+  }
+
+  suspend fun fetchImageReferencesForPost(postKey: String): List<StorageReference> {
+    return withContext(Dispatchers.Default) {
+      val storageRef = pictureDb.getReference("posts/$postKey")
+      val listResult = storageRef.listAll().await()
+      listResult.items
+    }
   }
 }
