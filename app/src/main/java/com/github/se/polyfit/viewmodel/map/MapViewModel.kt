f8c354d005ae3879fcd7ca3ebd3e04b52f6d070f
ben <benjamin.burki@epfl.ch>
2024-05-10 14:28:39 +0200

    fix assemble

--- app/src/main/java/com/github/se/polyfit/viewmodel/map/MapViewModel.kt
+++ app/src/main/java/com/github/se/polyfit/viewmodel/map/MapViewModel.kt
@@ -10,9 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MapViewModel
-@Inject
-constructor(private val repository: PostFirebaseRepository = PostFirebaseRepository()) :
+class MapViewModel @Inject constructor(private val repository: PostFirebaseRepository) :
     ViewModel() {
 
   private val _location = MutableLiveData<Location>()
