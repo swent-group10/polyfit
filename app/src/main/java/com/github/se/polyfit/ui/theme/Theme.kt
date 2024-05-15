2c4266f81ebd80cff3b62435ba9542536792a993
Antoine Cornaz <antomax33.jeux@gmail.com>
2024-05-15 15:59:02 +0200

    remove entirely dark mode

--- app/src/main/java/com/github/se/polyfit/ui/theme/Theme.kt
+++ app/src/main/java/com/github/se/polyfit/ui/theme/Theme.kt
@@ -11,9 +11,6 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme =
-    darkColorScheme(primary = Purple80, secondary = PurpleGrey80, tertiary = Pink80)
-
 private val LightColorScheme =
     lightColorScheme(
         primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40
