6a490c40b322a1b29f0373fbd6a316058fd6d770
sharif.bbd <sharif.bouabid@epfl.ch>
2024-05-28 16:12:54 +0200

    added tests

--- app/src/main/java/com/github/se/polyfit/data/api/FoodFactsAPI.kt
+++ app/src/main/java/com/github/se/polyfit/data/api/FoodFactsAPI.kt
@@ -4,62 +4,61 @@ import android.util.Log
 import co.yml.charts.common.extensions.roundTwoDecimal
 import com.github.se.polyfit.model.ingredient.Ingredient
 import com.github.se.polyfit.model.nutritionalInformation.MeasurementUnit
-import com.github.se.polyfit.model.nutritionalInformation.Nutrient
-import com.github.se.polyfit.model.nutritionalInformation.NutritionalInformation
+import java.io.IOException
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.io.IOException
 
 class OpenFoodFactsApi {
-    private val client = OkHttpClient()
-    private val BASE_URL = "https://world.openfoodfacts.net/api/v2/product/"
-    private val TAG = "OpenFoodFactsAPICaller"
-    private val URL_FIELDS = "?fields=product_name,nutriments,quantity"
+  private val client = OkHttpClient()
+  private var BASE_URL = "https://world.openfoodfacts.net/api/v2/product/"
+  private val TAG = "OpenFoodFactsAPICaller"
+  private val URL_FIELDS = "?fields=product_name,nutriments,quantity"
 
-    fun getFoodFacts(barcode: String): ProductResponseAPI? {
-        val request = Request.Builder()
-            .url(BASE_URL + barcode + URL_FIELDS)
-            .build()
+  fun setBaseUrl(url: String) {
+    BASE_URL = url
+  }
 
-        return try {
-            val response = client.newCall(request).execute()
-            if (!response.isSuccessful) {
-                Log.e(TAG, "Error in getting product facts: ${response.code}")
-                ProductResponseAPI(null, APIResponse.FAILURE)
-            } else {
-                response.body?.string()?.let {
-                    ProductResponseAPI(ProductResponseAPI.fromJson(it), APIResponse.SUCCESS)
-                }
-            }
-        } catch (e: IOException) {
-            Log.e(TAG, e.stackTraceToString())
-            null
+  fun getFoodFacts(barcode: String = "3017624010701"): ProductResponseAPI? {
+    val request = Request.Builder().url(BASE_URL + barcode + URL_FIELDS).build()
+
+    return try {
+      val response = client.newCall(request).execute()
+      if (!response.isSuccessful) {
+        Log.e(TAG, "Error in getting product facts: ${response.code}")
+        ProductResponseAPI(null, APIResponse.FAILURE)
+      } else {
+        response.body?.string()?.let {
+          ProductResponseAPI(ProductResponseAPI.fromJson(it), APIResponse.SUCCESS)
         }
+      }
+    } catch (e: IOException) {
+      Log.e(TAG, e.stackTraceToString())
+      throw IOException(e)
+    }
+  }
+
+  fun getIngredient(barcode: String): Ingredient {
+    var ingredient = Ingredient.default()
+
+    try {
+
+      val apiResponse = getFoodFacts(barcode)
+      if (apiResponse!!.status == APIResponse.SUCCESS) {
+        val productInfo = apiResponse.productResponse
+        val newIngredient =
+            Ingredient(
+                name = productInfo?.product!!.product_name,
+                amount = productInfo.product.quantity?.toDoubleOrNull() ?: 0.0,
+                id = 0,
+                unit = MeasurementUnit.G,
+                nutritionalInformation = productInfo.product.nutriments.getNutrientList())
+        ingredient = newIngredient
+      }
+    } catch (e: IOException) {
+      Log.e(TAG, "Unable to create Ingredient from information")
+      throw IOException(e)
     }
 
-    fun getIngredient(barcode : String) : Ingredient{
-        var ingredient = Ingredient.default()
-
-        try {
-
-            val apiResponse = getFoodFacts(barcode)
-            if(apiResponse!!.status == APIResponse.SUCCESS){
-                val productInfo = apiResponse.productResponse
-                val newIngredient =
-                    Ingredient(
-                        name = productInfo?.product!!.product_name,
-                        amount = productInfo.product.quantity?.toDouble()!!.roundTwoDecimal(),
-                        id = 0,
-                        unit = MeasurementUnit.G,
-                        nutritionalInformation = productInfo.product.nutriments.getNutrientList()
-                    )
-                ingredient = newIngredient
-            }
-        }catch (e : IOException){
-            Log.e(TAG, "Unable to create Ingredient from information")
-            throw IOException(e)
-        }
-
-        return ingredient
-    }
+    return ingredient
+  }
 }
--- app/src/main/java/com/github/se/polyfit/data/api/ProductInfoResponseAPI.kt
+++ app/src/main/java/com/github/se/polyfit/data/api/ProductInfoResponseAPI.kt
@@ -7,36 +7,36 @@ import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
-class ProductResponseAPI(val productResponse : ProductResponse?, val status : APIResponse){
-    companion object{
-        val moshi: Moshi by lazy{
-            Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        }
-        fun fromJson(jsonString : String) : ProductResponse{
-            val adapter = moshi.adapter(ProductResponse::class.java).lenient()
-            return adapter.fromJson(jsonString) ?: throw IllegalArgumentException("Invalid JSON data")
-        }
+class ProductResponseAPI(val productResponse: ProductResponse?, val status: APIResponse) {
+  companion object {
+    val moshi: Moshi by lazy { Moshi.Builder().add(KotlinJsonAdapterFactory()).build() }
+
+    fun fromJson(jsonString: String): ProductResponse {
+      val adapter = moshi.adapter(ProductResponse::class.java).lenient()
+      return adapter.fromJson(jsonString) ?: throw IllegalArgumentException("Invalid JSON data")
     }
+  }
 }
 
+@JsonClass(generateAdapter = true) data class ProductResponse(val product: Product)
+
 @JsonClass(generateAdapter = true)
-data class ProductResponse(val product: Product)
-@JsonClass(generateAdapter = true)
-data class Product(val product_name : String, val nutriments : Nutriments, val quantity : String?)
+data class Product(val product_name: String, val nutriments: Nutriments, val quantity: String?)
 
 @JsonClass(generateAdapter = true)
 data class Nutriments(
-    val fat : Double?,
-    val carbohydrates : Double?,
-    val sugars_value : Double?,
-    val proteins : Double?
-){
-    fun getNutrientList() : NutritionalInformation{
+    val fat: Double?,
+    val carbohydrates: Double?,
+    val sugars_value: Double?,
+    val proteins: Double?
+) {
+  fun getNutrientList(): NutritionalInformation {
 
-        val fatNutrient = Nutrient("fat", fat ?: 0.0, MeasurementUnit.G )
-        val carbohydratesNutrient = Nutrient("carbohydrates", carbohydrates ?: 0.0, MeasurementUnit.G)
-        val sugarNutrient = Nutrient("sugar", sugars_value ?: 0.0, MeasurementUnit.G)
-        val proteinsNutrient = Nutrient("protein", proteins ?: 0.0, MeasurementUnit.G)
-        return NutritionalInformation(mutableListOf(fatNutrient, carbohydratesNutrient, sugarNutrient, proteinsNutrient))
-    }
+    val fatNutrient = Nutrient("fat", fat ?: 0.0, MeasurementUnit.G)
+    val carbohydratesNutrient = Nutrient("carbohydrates", carbohydrates ?: 0.0, MeasurementUnit.G)
+    val sugarNutrient = Nutrient("sugar", sugars_value ?: 0.0, MeasurementUnit.G)
+    val proteinsNutrient = Nutrient("protein", proteins ?: 0.0, MeasurementUnit.G)
+    return NutritionalInformation(
+        mutableListOf(fatNutrient, carbohydratesNutrient, sugarNutrient, proteinsNutrient))
+  }
 }
