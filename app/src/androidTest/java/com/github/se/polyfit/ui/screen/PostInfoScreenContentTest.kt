04d65a657a74211551bdd555f8455d886e9e19c0
Neb <56965385+Neb1010@users.noreply.github.com>
2024-05-31 23:29:45 +0200

    fix test

--- app/src/androidTest/java/com/github/se/polyfit/ui/screen/PostInfoScreenContentTest.kt
+++ app/src/androidTest/java/com/github/se/polyfit/ui/screen/PostInfoScreenContentTest.kt
@@ -13,7 +13,9 @@ import com.github.se.polyfit.model.post.Post
 import com.github.se.polyfit.viewmodel.meal.MealViewModel
 import com.github.se.polyfit.viewmodel.post.ViewPostViewModel
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
 import java.time.LocalDate
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Before
@@ -22,69 +24,70 @@ import org.junit.Test
 
 class PostInfoScreenContentTest {
 
-  @get:Rule val composeTestRule = createComposeRule()
+    @get:Rule val composeTestRule = createComposeRule()
 
-  private lateinit var viewPostViewModel: ViewPostViewModel
+    private lateinit var viewPostViewModel: ViewPostViewModel
 
-  @Before
-  fun setup() {
-    viewPostViewModel = mockk<ViewPostViewModel>(relaxed = true)
-    every { viewPostViewModel.isFetching } returns MutableStateFlow(false)
-  }
+    @Before
+    fun setup() {
+        viewPostViewModel = mockk<ViewPostViewModel>(relaxed = true)
+        every { viewPostViewModel.isFetching } returns MutableLiveData(false)
+    }
 
-  @Test
-  fun PostInfoScreen_displays_post_information() {
-    val mealViewModel = mockk<MealViewModel>(relaxed = true)
-    val post = Post.default()
-    every { viewPostViewModel.getAllPost() } returns MutableLiveData(listOf(post))
-    post.meal.addIngredient(
-        Ingredient(
-            "ingredient1",
-            100,
-            100.0,
-            MeasurementUnit.G,
-            NutritionalInformation(
-                mutableListOf(
-                    Nutrient("Protein", 12.0, MeasurementUnit.G),
-                    Nutrient("Carbohydrates", 21.50, MeasurementUnit.G),
-                    Nutrient("Fats", 25.0, MeasurementUnit.G),
-                    Nutrient("Vitamins", 40.0, MeasurementUnit.G),
-                ))))
-    post.description = "Meal of the day, with a lot of nutrients and vitamins"
-    val posts = listOf(post)
+    @Test
+    fun PostInfoScreen_displays_post_information() {
+        val mealViewModel = mockk<MealViewModel>(relaxed = true)
+        val post = Post.default()
+        every { viewPostViewModel.posts } returns MutableStateFlow(listOf(post))
+        every { viewPostViewModel.getNearbyPosts() } just runs
+        post.meal.addIngredient(
+            Ingredient(
+                "ingredient1",
+                100,
+                100.0,
+                MeasurementUnit.G,
+                NutritionalInformation(
+                    mutableListOf(
+                        Nutrient("Protein", 12.0, MeasurementUnit.G),
+                        Nutrient("Carbohydrates", 21.50, MeasurementUnit.G),
+                        Nutrient("Fats", 25.0, MeasurementUnit.G),
+                        Nutrient("Vitamins", 40.0, MeasurementUnit.G),
+                    ))))
+        post.description = "Meal of the day, with a lot of nutrients and vitamins"
+        val posts = listOf(post)
 
-    every { mealViewModel.meal } returns MutableStateFlow(Meal.default())
-    every { mealViewModel.isComplete } returns MutableStateFlow(true)
+        every { mealViewModel.meal } returns MutableStateFlow(Meal.default())
+        every { mealViewModel.isComplete } returns MutableStateFlow(true)
 
-    composeTestRule.setContent { PostInfoScreenContent(viewPostViewModel = viewPostViewModel) }
+        composeTestRule.setContent { PostInfoScreenContent(viewPostViewModel = viewPostViewModel) }
 
-    composeTestRule
-        .onNodeWithTag("DescriptionTitle")
-        .assertExists()
-        .assertTextContains("Description")
-    composeTestRule
-        .onNodeWithTag("Description")
-        .assertExists()
-        .assertTextContains("Meal of the day, with a lot of nutrients and vitamins")
-    composeTestRule
-        .onNodeWithTag("Date")
-        .assertExists()
-        .assertTextContains(LocalDate.now().dayOfMonth.toString(), substring = true)
-        .assertTextContains(LocalDate.now().month.toString(), substring = true)
-    composeTestRule.onNodeWithTag("NutrientTitle").assertExists()
-  }
+        composeTestRule
+            .onNodeWithTag("DescriptionTitle")
+            .assertExists()
+            .assertTextContains("Description")
+        composeTestRule
+            .onNodeWithTag("Description")
+            .assertExists()
+            .assertTextContains("Meal of the day, with a lot of nutrients and vitamins")
+        composeTestRule
+            .onNodeWithTag("Date")
+            .assertExists()
+            .assertTextContains(LocalDate.now().dayOfMonth.toString(), substring = true)
+            .assertTextContains(LocalDate.now().month.toString(), substring = true)
+        composeTestRule.onNodeWithTag("NutrientTitle").assertExists()
+    }
 
-  @Test
-  fun PostInfoScreen_displays_no_post_available_when_there_are_no_posts() {
-    val posts = listOf<Post>()
+    @Test
+    fun PostInfoScreen_displays_no_post_available_when_there_are_no_posts() {
+        val posts = listOf<Post>()
 
-    composeTestRule.setContent { PostInfoScreenContent(viewPostViewModel = viewPostViewModel) }
+        composeTestRule.setContent { PostInfoScreenContent(viewPostViewModel = viewPostViewModel) }
 
-    composeTestRule
-        .onNodeWithTag("NoPostText")
-        .assertExists()
-        .assertTextContains("Unfortunately there is no post", substring = true)
-        .assertExists()
-        .assertTextContains("available around you", substring = true)
-  }
+        composeTestRule
+            .onNodeWithTag("NoPostText")
+            .assertExists()
+            .assertTextContains("Unfortunately there is no post", substring = true)
+            .assertExists()
+            .assertTextContains("available around you", substring = true)
+    }
 }
